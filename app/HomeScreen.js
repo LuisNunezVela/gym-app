5771ab4bef86a8e30a2de849d8a1f57919e43f63
Luis Fernando Nuñez Vela <luisfernando.nvcb@gmail.com>
2024-11-25 05:13:32 -0400

    added packages and navbar page

--- app/HomeScreen.js
+++ app/HomeScreen.js
@@ -2,20 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import "../global.css"
 
-export default function App() {
+export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text className="text-red-500">Open up App.js</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
